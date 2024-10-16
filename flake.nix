9af9c6af36fcecb0b902599d43bfaead2ca0e71b
fanshi1028 <jackychany321@gmail.com>
2024-10-16 14:35:11 +0100

    nix flake update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     nix-github-actions = {
       url = "github:nix-community/nix-github-actions";
       inputs.nixpkgs.follows = "nixpkgs";
